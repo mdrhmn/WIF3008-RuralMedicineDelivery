ac8d483103c3a2078b0cd5831f64d9231630347b
Muhd Rahiman <50654608+mdrhmn@users.noreply.github.com>
2021-12-09 19:26:46 +0800

    Added new routes.py file for storing default checkpoint and paths

---
+++ routes.py
@@ -0,0 +1,6 @@
+class Route:
+
+    checkpoint = [[1, "ccw", 90, 1, "forward", 100, 5], [2, "ccw", 90, 1, "forward", 80, 4],
+                  [3, "ccw", 90, 1, "forward", 40, 2], [
+                  4, "cw", 90, 1, "forward", 60, 3],
+                  [5, "ccw", 90, 1, "forward", 40, 2], [0, "ccw", 90, 1, "forward", 40, 2]]
